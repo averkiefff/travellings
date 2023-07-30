0267490c777ff186994da356e6e5fd66862731f4
Aleksandr <averkiev.a-averkieff@yandex.ru>
2023-07-30 14:27:42 +0300

    vue2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createProvider } from './vue-apollo'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .use(vuetify)
+    .use(router)
+    .mount('#app')
+
+new Vue({
+    apolloProvider: createProvider(),
+    render: h => h(App)
+}).$mount('#app');
